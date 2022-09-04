be3e9af908eac494409e34c7a270974123afa063
yermary <ycag220593@gmail.com>
2022-09-04 20:10:14 -0300

    Creacion de los 3 formularios para insertar datos

--- AppsServicios/urls.py
+++ AppsServicios/urls.py
@@ -1,15 +1,14 @@
 #from django.contrib import path
 from django.urls import path
-#from AppsServicios.views import listar_Servicios
-#from AppsServicios.views import listar_tecnologias
-#from AppsServicios.views import lista_contactos
 from AppsServicios import views
 
 urlpatterns =[
     
     path('',views.inicio, name = "inicio"),
-    path('tecnologias/',views.tecnologias, name= "tecnologias"),
+    path('tecnologias/',views.tecnologia, name= "tecnologias"),
     path('contactos/',views.contactos, name= "contactos"),
     path('crear-tecnologia/',views.tecnologia_formulario, name= "tecnologia_formulario"),
+    path('crear-servicio/',views.servicio_formulario, name= "servicio_formulario"),
+    path('crear-contacto/',views.contacto_formulario, name= "contacto_formulario"),
 ]
 
--- AppsServicios/views.py
+++ AppsServicios/views.py
@@ -1,10 +1,13 @@
 # Create your views here.
+from ensurepip import version
 from typing import Dict
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template,Context, loader
 from AppsServicios.models import Tecnologias
 from AppsServicios.models import Contactos
+from AppsServicios.models import Servicios
+#from .AppsServicios.forms import NuevaTecnologia
 
 #Views para el inicio
 
@@ -18,16 +21,49 @@ def contactos(request):
 
     #Lista de tecnologias
 
-def tecnologias(request):
-    tecnologias= Tecnologias.objects.all()    
-    return render(request, "AppsServicios/lista_tecnologia.html", {'tecnologias': tecnologias})
+def tecnologia(request):
+    Tecnologia = Tecnologias.objects.all()    
+    return render(request, "AppsServicios/lista_tecnologia.html")
 
-def tecnologia_formulario (request):
-    if request.method =="POST":
+def tecnologia_formulario(request):
+
+    if request.method == 'POST':
         data_formulario: Dict = request.POST
-        tecnologia= Tecnologias(nombre=data_formulario['nombre'], version=data_formulario['version'])
-        Tecnologias.save
-        return render(request, "AppsServicios/inicio.html")
+        tecnologia = Tecnologias(nombre=data_formulario['nombre'], version=data_formulario['version'])
+        tecnologia.save()
+
+        return render(request, 'AppsServicios/inicio.html')
     else:
-        return render(request, "AppsServicios/form_tecnologias.html")
-    
+        return render(request, 'AppsServicios/form_tecnologias.html')
+
+
+def servicio(request):
+    Servicio = Servicios.objects.all()    
+    return render(request, "AppsServicios/lista_servicios.html")
+
+
+def servicio_formulario(request):
+
+    if request.method == 'POST':
+        data_formulario: Dict = request.POST
+        servicio = Servicios(nombre=data_formulario['nombre'], tiempo=data_formulario['tiempo'], valor=data_formulario['valor'])
+        servicio.save()
+
+        return render(request, 'AppsServicios/inicio.html')
+    else:
+        return render(request, 'AppsServicios/form_servicios.html')
+
+def contacto(request):
+    Contacto = Contactos.objects.all()    
+    return render(request, "AppsServicios/lista_contacto.html")
+
+def contacto_formulario(request):
+
+    if request.method == 'POST':
+        data_formulario: Dict = request.POST
+        contacto = Contactos(nombre=data_formulario['nombre'], apellido=data_formulario['apellido'], email=data_formulario['email'])
+        contacto.save()
+
+        return render(request, 'AppsServicios/inicio.html')
+    else:
+        return render(request, 'AppsServicios/form_contacto.html')
